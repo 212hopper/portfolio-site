31623b64c627e5c24ef2e165fb082fa347b0ee1f
212hopper <158082447+212hopper@users.noreply.github.com>
2026-07-03 12:18:38 -0500

    Adding Content

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,19 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
